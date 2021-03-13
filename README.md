9ac921d04d6dbec73689869aeeacfd66c842b661
Daniel Melody <dmwalter12@gmail.com>
2021-03-13 15:08:59 -0500

    chapter 2 complete

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # hecto-clone
 
 going through Philipp Flenker's blog series on `hecto` -- a small text editor
+
+## tidbits
+
+`\r` is a *carriage return* which moves the cursor back to the beginning of the line
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn die(e: std::io::Error) {
+    panic!(e);
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Enter raw mode
+    let _stdout = stdout().into_raw_mode().unwrap();
+    // Read the input
+    for b in io::stdin().bytes() {
+        //* variable shadowing
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                // check if `c` is a control character
+                // * control character = non-printable character -- ie. ASCII 0-31
+                if c.is_control() {
+                    println!("{:?} \r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
+        }
+    }
 }
