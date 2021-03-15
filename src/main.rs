1ae8528f187b3344e55d15c919f1a83718d64214
Daniel Melody <dmwalter12@gmail.com>
2021-03-14 22:05:19 -0400

    chapter 3 complete

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,11 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+mod terminal;
 
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
-
-fn die(e: std::io::Error) {
-    panic!(e);
-}
+use editor::Editor;
+pub use editor::Position;
+pub use terminal::Terminal;
 
 fn main() {
-    // Enter raw mode
-    let _stdout = stdout().into_raw_mode().unwrap();
-    // Read the input
-    for b in io::stdin().bytes() {
-        //* variable shadowing
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                // check if `c` is a control character
-                // * control character = non-printable character -- ie. ASCII 0-31
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
-                }
-                if b == to_ctrl_byte('q') {
-                    break;
-                }
-            }
-            Err(err) => die(err),
-        }
-    }
+    Editor::default().run();
 }
